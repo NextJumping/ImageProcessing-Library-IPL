6f10876e915671a6b32849f46e179aea908f3087
NextJumping <nextjumping@proton.me>
2020-04-14 15:09:18 +0000

    Updated installation instructions

---
+++ IPL.bat
@@ -0,0 +1,15 @@
+
+
+setlocal & pushd
+
+set PKG=%CD%\..\..\pkg
+set QTDIR=%PKG%\qt-everywhere-opensource-src-4.7.3
+set CURL_DIR=%PKG%\curl-7.21.7
+set LIBPNG_DIR=%PKG%\lpng1510
+set LIBFFMPEG_DIR=%PKG%\ffmpeg-git-41a097a-win64-dev
+
+set IPL=%CD%
+
+start "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" %CD%/IPL.sln /useenv
+
+popd & endlocal
