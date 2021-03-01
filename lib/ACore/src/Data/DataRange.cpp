1340667498abd462497739f0ca80cf974956ce56
NextJumping <nextjumping@proton.me>
2021-03-01 09:28:01 +0000

    update fr-CA translation

--- lib/ACore/src/Data/DataRange.cpp
+++ lib/ACore/src/Data/DataRange.cpp
@@ -12,4 +12,28 @@ const I1  DataRange_I1::max =  127i8;
 const I1u DataRange_I1u::min = 0x00ui8;
 const I1u DataRange_I1u::max = 0xFFui8;
 
-const
+const I2  DataRange_I2::min = -32768;
+const I2  DataRange_I2::max =  32767i16;
+
+const I2u DataRange_I2u::min = 0x0000ui16;
+const I2u DataRange_I2u::max = 0xFFFFui16;
+
+const I4  DataRange_I4::min = -2147483648i32;
+const I4  DataRange_I4::max =  2147483647i32;
+
+const I4u DataRange_I4u::min = 0x00000000ui32;
+const I4u DataRange_I4u::max = 0xFFFFFFFFui32;
+
+const I2  DataRange_I2_I1u::min = 0x00ui16;
+const I2  DataRange_I2_I1u::max = 0xFFui16;
+
+const I2  DataRange_I2_I1::min = -128i16;
+const I2  DataRange_I2_I1::max =  127i16;
+
+const I2u DataRange_I2u_I1u::min = 0x00ui16;
+const I2u DataRange_I2u_I1u::max = 0xFFui16;
+
+const I4  DataRange_I4_I1::min = -128i32;
+const I4  DataRange_I4_I1::max =  127i32;
+
+const I4  DataRange_I4_I1u::min = 0
