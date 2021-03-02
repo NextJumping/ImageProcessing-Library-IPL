93170cf8801357759fce2bbbaea9a7c402d43937
NextJumping <nextjumping@proton.me>
2021-03-02 22:18:58 +0000

    Version bump to 0.0.8

--- lib/ACore/src/Data/DataRange.h
+++ lib/ACore/src/Data/DataRange.h
@@ -34,4 +34,24 @@ template <
 };
 
 typedef DataRange_Base<B1 > DataRange_B1 ;
-typedef Dat
+typedef DataRange_Base<I1 > DataRange_I1 ;
+typedef DataRange_Base<I1u> DataRange_I1u;
+typedef DataRange_Base<I2 > DataRange_I2 ;
+typedef DataRange_Base<I2u> DataRange_I2u;
+typedef DataRange_Base<I4 > DataRange_I4 ;
+typedef DataRange_Base<I4u> DataRange_I4u;
+
+typedef DataRange_BaseDepth<I2 ,1> DataRange_I2_I1  ;
+typedef DataRange_BaseDepth<I2 ,2> DataRange_I2_I1u ;
+
+typedef DataRange_BaseDepth<I2u,1> DataRange_I2u_I1u;
+
+typedef DataRange_BaseDepth<I4 ,1> DataRange_I4_I1  ;
+typedef DataRange_BaseDepth<I4 ,2> DataRange_I4_I1u ;
+typedef DataRange_BaseDepth<I4 ,3> DataRange_I4_I2  ;
+typedef DataRange_BaseDepth<I4 ,4> DataRange_I4_I2u ;
+
+typedef DataRange_BaseDepth<I4u,1> DataRange_I4u_I1u;
+typedef DataRange_BaseDepth<I4u,2> DataRange_I4u_I2u;
+
+typedef DataRange_BaseDepth<F4
