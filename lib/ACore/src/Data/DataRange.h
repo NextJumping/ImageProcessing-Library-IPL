2f91520cea3eebb90c1b79d9cc0ae41636557beb
NextJumping <nextjumping@proton.me>
2021-03-02 12:36:33 +0000

    license information

--- lib/ACore/src/Data/DataRange.h
+++ lib/ACore/src/Data/DataRange.h
@@ -8,4 +8,30 @@
 
 namespace Data {
 
-//TODO: This is de
+//TODO: This is designed to be used as constexpr (but compiler support hasn't caught up yet)
+//      - when the compiler supports constexpr -> replace const with constexpr
+template <
+	typename NumberType
+> class ACORE_DLL_EXPORT DataRange_Base {
+	public:
+		static const NumberType min;
+		static const NumberType max;
+
+		static FINLINE const NumberType & getMin() {return min;}
+		static FINLINE const NumberType & getMax() {return max;}
+};
+
+template <
+	typename NumberType,
+	int depthID
+> class ACORE_DLL_EXPORT DataRange_BaseDepth {
+	public:
+		static const NumberType min;
+		static const NumberType max;
+
+		static FINLINE const NumberType & getMin() {return min;}
+		static FINLINE const NumberType & getMax() {return max;}
+};
+
+typedef DataRange_Base<B1 > DataRange_B1 ;
+typedef Dat
