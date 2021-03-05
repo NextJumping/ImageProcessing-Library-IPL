35b696c0f90bda0dc47d6f133a74b15871e38616
NextJumping <nextjumping@proton.me>
2021-03-05 06:54:26 +0000

    Lines

--- lib/ACore/src/Data/Float.h
+++ lib/ACore/src/Data/Float.h
@@ -26,4 +26,38 @@ class ACORE_DLL_EXPORT F16c { //TODO: Complete a quad precision floating point c
 };
 
 class ACORE_DLL_EXPORT F8c {
-	pu
+	public:
+		union {
+			F8n f;
+			I8u u;
+			struct {
+				I8u mantissa : 52;
+				I8u exponent : 11;
+				I8u sign : 1;
+			} s ;
+		};
+		FINLINE          F8c(){}
+		FINLINE          F8c(const F8n & _f):f(_f){}
+		FINLINE explicit F8c(const I8u & _u,const B1 & dummy):u(_u){}
+		FINLINE operator F8n(){return f;}
+
+	    FINLINE B1  sign()     const { return (u >> 63) != 0; }
+		FINLINE I8u mantissa() const { return  u & ((1i64 << 52) - 1); }
+		FINLINE I8u exponent() const { return (u >> 52) & 0x7FF; }
+
+};
+
+class ACORE_DLL_EXPORT F4c {
+	public:
+		union {
+			F4n f;
+			I4u u;
+			struct {
+				I4u mantissa : 23;
+				I4u exponent : 8;
+				I4u sign : 1;
+			} s ;
+		};
+		FINLINE          F4c(){}
+		FINLINE          F4c(const F4n & _f):f(_f){}
+		FINLINE explicit F4c(const I4u & _u,
