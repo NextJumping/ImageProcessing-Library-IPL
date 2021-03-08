614f7fe9b36eedad9436a64a97c72be6013a891f
NextJumping <nextjumping@proton.me>
2021-03-08 09:18:57 +0000

    add users

--- lib/ACore/src/Meta/TypeVector_cpp.h
+++ lib/ACore/src/Meta/TypeVector_cpp.h
@@ -8,4 +8,26 @@ namespace Meta {
 #define TYPE_DEF TypeVector<ElementType,Length>
 
 TEMPLATE_DEF void TYPE_DEF::setAs(const ThisType    &_vector){value=_vector.value;parent.setAs(_vector.parent);}
-TEMPLATE_DEF void TYPE_DEF::setAs(const ElementType &_scalar){value=_scalar      ;parent.setAs(_s
+TEMPLATE_DEF void TYPE_DEF::setAs(const ElementType &_scalar){value=_scalar      ;parent.setAs(_scalar);}
+
+TEMPLATE_DEF void TYPE_DEF::setAsMin(const ThisType &vector){
+	if(vector.value<value){value=vector.value;}
+	parent.setAsMin(vector.parent);
+}
+TEMPLATE_DEF void TYPE_DEF::setAsMax(const ThisType &vector){
+	if(vector.value>value){value=vector.value;}
+	parent.setAsMax(vector.parent);
+}
+TEMPLATE_DEF void TYPE_DEF::setAsMin(const ElementType &scalar){
+	if(scalar<value){value=scalar;}
+	parent.setAsMin(scalar.parent);
+}
+TEMPLATE_DEF void TYPE_DEF::setAsMax(const ElementType &scalar){
+	if(scalar>value){value=scalar;}
+	parent.setAsMax(scalar.parent);
+}
+
+// Operators:
+//With Scalar
+TEMPLATE_DEF TYPE_DEF &TYPE_DEF::operator  =(const ElementType &_scalar){value=_scalar;parent=_scalar;return (*this);}
+TEMPLATE_DEF TYPE_DEF::TypeVector(const ElementType
