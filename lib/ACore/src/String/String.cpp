ad8ed8a94bd0eb543d10c7118ce4723b0ee14102
NextJumping <nextjumping@proton.me>
2021-03-15 09:45:58 +0000

    MRG

--- lib/ACore/src/String/String.cpp
+++ lib/ACore/src/String/String.cpp
@@ -5,4 +5,26 @@
 
 namespace String {
 
-//TODO: Rewrite these 
+//TODO: Rewrite these functions to not use sstream - it is slow
+ACORE_DLL_EXPORT std::string getString(const I4  & number){
+	std::stringstream stream;
+	stream<<number;
+	return stream.str();
+}
+ACORE_DLL_EXPORT std::string getString(const I8  & number){
+	std::stringstream stream;
+	stream<<number;
+	return stream.str();
+}
+ACORE_DLL_EXPORT std::string getString(const I8u & number){
+	std::stringstream stream;
+	stream<<number;
+	return stream.str();
+}
+ACORE_DLL_EXPORT std::string getString(const I4  & number,const I4 & zeros){
+	std::stringstream stream;
+	stream<<std::setw(zeros)<<std::setfill('0')<<number;
+	return stream.str();
+}
+ACORE_DLL_EXPORT std::string getString(const I8  & number,const I4 & zeros){
+	std::stringstream str
