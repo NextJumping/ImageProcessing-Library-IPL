7f3e65a3759be85bd77cc48d806b8ed5c023daf0
NextJumping <nextjumping@proton.me>
2021-03-18 09:41:38 +0000

    adapted test

--- lib/ACore/src/String/String.cpp
+++ lib/ACore/src/String/String.cpp
@@ -27,4 +27,26 @@ ACORE_DLL_EXPORT std::string getString(const I4  & number,const I4 & zeros){
 	return stream.str();
 }
 ACORE_DLL_EXPORT std::string getString(const I8  & number,const I4 & zeros){
-	std::stringstream str
+	std::stringstream stream;
+	stream<<std::setw(zeros)<<std::setfill('0')<<number;
+	return stream.str();
+}
+ACORE_DLL_EXPORT std::string getString(const I8u & number,const I4 & zeros){
+	std::stringstream stream;
+	stream<<std::setw(zeros)<<std::setfill('0')<<number;
+	return stream.str();
+}
+
+ACORE_DLL_EXPORT std::string getString(const F4 & number){
+	std::stringstream stream;
+	stream<< std::setprecision( 8)<<number;//TODO: Check that this is enough precision
+	return stream.str();
+}
+ACORE_DLL_EXPORT std::string getString(const F8 & number){
+	std::stringstream stream;
+	stream<< std::setprecision(15)<<number;//TODO: Check that this is enough precision
+	return stream.str();
+}
+
+ACORE_DLL_EXPORT bool hasEnding(const std::string & fullString, const std::string & ending){
+    if(fullString.length() >= ending.leng
