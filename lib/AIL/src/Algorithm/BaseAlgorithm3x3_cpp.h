a6799c57e0dc2bc13430ca66d493526a18f2decf
NextJumping <nextjumping@proton.me>
2021-03-22 16:17:47 +0000

    coverage

--- lib/AIL/src/Algorithm/BaseAlgorithm3x3_cpp.h
+++ lib/AIL/src/Algorithm/BaseAlgorithm3x3_cpp.h
@@ -10,4 +10,23 @@ template <
 	typename AlgorithmType,
 	typename PixelDataType
 > void BaseAlgorithm3x3 (
-	const Image::ImageView<PixelD
+	const Image::ImageView<PixelDataType> & srcImage,
+	Image::ImageView<PixelDataType> & dstImage)
+{
+	if(srcImage.getSize()!=dstImage.getSize()){return;}
+
+	if((srcImage.isSimpleView()==true)&&(dstImage.isSimpleView()==true)){
+		auto srcImageHeightm1 = srcImage.getHeight()-1;
+
+		const auto & srcImageWidth   = srcImage.getWidth();
+			  auto   srcImageWidthm1 = srcImage.getWidth()-1;
+			  auto   srcImageWidthp1 = srcImage.getWidth()+1;
+
+		auto srcImageDataPtr = srcImage.getDataPtr();
+		auto dstImageDataPtr = dstImage.getDataPtr();
+	
+		I4 y = 0; // TODO: Write these loops to get rid of x and y ... and make sure that the complier still unrolls the loops
+		I4 x = 0;
+
+		//First Row - First Pixel
+		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr              ,srcIm
