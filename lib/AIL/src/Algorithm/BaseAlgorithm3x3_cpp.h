a7ee7eeadfa31733f21c6d01c10326d0f9eb544b
NextJumping <nextjumping@proton.me>
2021-04-10 18:54:10 +0000

    experience

--- lib/AIL/src/Algorithm/BaseAlgorithm3x3_cpp.h
+++ lib/AIL/src/Algorithm/BaseAlgorithm3x3_cpp.h
@@ -221,4 +221,17 @@ template <
 					++srcImageDataPtr;
 				}
 				//Inside Row - Last Pixel
-				AlgorithmType::process(dstImageDa
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStride,
+													   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr              ,
+													   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStride);
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+				srcImageDataPtr+=srcImage.getNumPixelsBetweenRows();
+			}
+		}
+
+		if(srcImage.hasYendSection()==true){
+			if(srcImage.hasXbeginSection()==true){
+				//Last Row - First pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStride,srcImageDataPt
