277af3d0e219fb3c33085adb95512b24f60eb8b8
NextJumping <nextjumping@proton.me>
2021-04-16 20:56:36 +0000

    Fixed a minor typo.

--- lib/AIL/src/Algorithm/BasicWx1baseAlgorithmParametersType.h
+++ lib/AIL/src/Algorithm/BasicWx1baseAlgorithmParametersType.h
@@ -1,3 +1,36 @@
 
 #ifndef IMAGE__BasicWx1baseAlgorithmParametersType_H
-#define IMAGE__BasicWx1ba
+#define IMAGE__BasicWx1baseAlgorithmParametersType_H
+
+#include "AIL.h"
+
+namespace Algorithm {
+
+class AIL_DLL_EXPORT BasicWx1baseAlgorithmParametersType {
+	public:
+
+		BasicWx1baseAlgorithmParametersType(
+			const I4 & _xOffset,
+			const I4 & _filterWidth
+		)
+			:xOffset(_xOffset)
+			,filterWidth(_filterWidth)
+		{
+		}
+
+		BasicWx1baseAlgorithmParametersType(
+			const BasicWx1baseAlgorithmParametersType & _other
+		)
+			:xOffset(_other.xOffset)
+			,filterWidth(_other.filterWidth)
+		{
+		}
+
+		const I4 xOffset;
+		const I4 filterWidth;
+
+};
+
+}
+
+#endif
---
+++ lib/AIL/src/Filter/BaseBoxFilterAlgorithm.h
@@ -0,0 +1,11 @@
+
+#ifndef FILTER__BaseBoxFilterAlgorithm_H
+#define FILTER__BaseBoxFilterAlgorithm_H
+
+#include <Algorithm/BaseOperationTempType.h>
+
+namespace Filter {
+
+template <
+	typename PixelDataType,
+	typename PixelComputati
