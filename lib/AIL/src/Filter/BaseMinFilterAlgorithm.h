27c3a9cf1ece4faeacf28fd677f7c2d1342ea6e3
NextJumping <nextjumping@proton.me>
2021-04-20 16:52:05 +0000

    Version bump to 0.4.9

--- lib/AIL/src/Filter/BaseMinFilterAlgorithm.h
+++ lib/AIL/src/Filter/BaseMinFilterAlgorithm.h
@@ -1,3 +1,36 @@
 
 #ifndef FILTER__BaseMinFilterAlgorithm_H
-#define FILT
+#define FILTER__BaseMinFilterAlgorithm_H
+
+namespace Filter {
+
+template <
+	typename PixelDataType,
+	typename PixelComputationType,
+	typename ParametersType,
+	typename TempType
+> class AIL_DLL_EXPORT BaseMinFilterAlgorithm {
+	public:
+		static FINLINE void initial(
+			TempType & tempData,
+			const ParametersType & parameters)
+		{
+		}
+
+		static FINLINE void first(
+			TempType & tempData,
+			const ParametersType & parameters,
+			const PixelComputationType & srcImageData)
+		{
+			tempData.tempPixel = srcImageData;
+		}
+
+		static FINLINE void inner(
+			TempType & tempData,
+			const ParametersType & parameters,
+			const PixelComputationType & srcImageData)
+		{
+			tempData.tempPixel.setAsMin(srcImageData);
+		}
+
+		static FI
