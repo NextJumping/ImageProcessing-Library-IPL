28c0fddd7597499f3bf64e8c70d39f1f2e9fab98
NextJumping <nextjumping@proton.me>
2021-04-21 13:08:17 +0000

    staging: vt6655: Fix Warning on boot handle_irq_event_percpu.

--- lib/AIL/src/Filter/BlackTopHatFilter.h
+++ lib/AIL/src/Filter/BlackTopHatFilter.h
@@ -17,4 +17,16 @@ template <
 	public:
 		BlackTopHatFilter():iterations(3),type(MorphKernelType::square){};
 		BlackTopHatFilter(const I4 & _iterations):iterations(_iterations),type(MorphKernelType::square){};
-		BlackTopHatFilter(const I4 & _iterations,const MorphKernelType & _type):iterations(_iterations),type(_t
+		BlackTopHatFilter(const I4 & _iterations,const MorphKernelType & _type):iterations(_iterations),type(_type){};
+
+		void applyTo(const Image::Image<PixelType> & srcImage,Image::Image<PixelType> & dstImage) const ;
+
+	private:
+		I4 iterations;
+		MorphKernelType type;
+
+};
+
+}
+
+#endif
