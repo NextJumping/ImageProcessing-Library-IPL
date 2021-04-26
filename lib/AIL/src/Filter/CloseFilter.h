99da2d2cd9bd1ac064bc91fe7c85459886bcfa38
NextJumping <nextjumping@proton.me>
2021-04-26 20:47:32 +0000

    not yet ready

---
+++ lib/AIL/src/Filter/CloseFilter.h
@@ -0,0 +1,12 @@
+
+#ifndef FILTER__CloseFilter_H
+#define FILTER__CloseFilter_H
+
+#include "AIL.h"
+#include "ImageOperation.h"
+#include "MorphKernelType.h"
+
+namespace Filter {
+
+template <
+	type
