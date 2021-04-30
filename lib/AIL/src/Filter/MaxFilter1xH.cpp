ce150ca7f414ea5ff5ea9e723ebe2e9cbc4dfcb5
NextJumping <nextjumping@proton.me>
2021-04-30 11:13:47 +0000

    floppy: do put_disk on current dr if blk_init_queue fails

--- lib/AIL/src/Filter/MaxFilter1xH.cpp
+++ lib/AIL/src/Filter/MaxFilter1xH.cpp
@@ -3,4 +3,25 @@
 #include <Algorithm/Algorithm1xH.h>
 #include "BaseMaxFilterAlgorithm.h"
 #include "SimpleWxHbooleanOperationParametersType.h"
-#include "Simple1xHbo
+#include "Simple1xHbooleanOperationBaseAlgorithm.h"
+#include <Algorithm/BaseOperationTempType.h>
+
+namespace Filter {
+
+template <
+	typename PixelType
+> void AIL_DLL_EXPORT MaxFilter1xH<PixelType>::applyTo(const Image::Image<PixelType> & srcImage,Image::Image<PixelType> & dstImage) const {
+
+	typedef typename PixelType::DataType        PixelDataType;
+	typedef typename PixelType::ComputationType PixelComputationType;
+
+	filterData.calculateKernelSkipData(srcImage.getWidth());
+
+	SimpleWxHbooleanOperationParametersType parameters(getFilterData(),getXoffset(),getYoffset());
+
+	Algorithm::Algorithm1xH<
+		Simple1xHbooleanOperationBaseAlgorithm<
+			BaseMaxFilterAlgorithm<
+				PixelDataType,
+				PixelComputationType,
+				SimpleWxHbooleanOperationParameters
