37fddb77f90bfb5d56aeaf425b60e5876cbdcf17
NextJumping <nextjumping@proton.me>
2021-05-01 18:03:07 +0000

    timestamp filler

--- lib/AIL/src/Filter/MaxFilterWx1basic.h
+++ lib/AIL/src/Filter/MaxFilterWx1basic.h
@@ -1,2 +1,43 @@
 
-#ifndef FILTER__MaxFilterWx1
+#ifndef FILTER__MaxFilterWx1basic_H
+#define FILTER__MaxFilterWx1basic_H
+
+#include "BaseMaxFilter.h"
+
+namespace Filter {
+
+template <
+	typename PixelType
+> class AIL_DLL_EXPORT MaxFilterWx1basic {
+
+	public:
+		MaxFilterWx1basic(
+			const I4 & _filterWidth
+		)
+			:filterWidth(_filterWidth)	
+			,xOffset( (_filterWidth - 1) / 2 )
+		{
+		};
+		MaxFilterWx1basic(
+			const I4 & _filterWidth,
+			const I4 & _xOffset
+		)
+			:filterWidth(_filterWidth)
+			,xOffset(_xOffset)
+		{
+		};
+
+		FINLINE const I4 & getWidth()   const {return filterWidth;}
+		FINLINE const I4 & getXoffset() const {return xOffset;}
+
+		void applyTo(const Image::Image<PixelType> & srcImage,Image::Image<PixelType> & dstImage) const ;
+
+	private:
+		const I4 filterWidth;
+		const I4 xOffset;
+
+};
+
+}
+
+#endif
---
+++ lib/AIL/src/Filter/MaxFilterWxH.cpp
@@ -0,0 +1,44 @@
+
+
+#include "MaxFilterWxH.h"
+#include <Algorithm/AlgorithmWxH.h>
+#include "BaseMaxFilterAlgorithm.h"
+#include "SimpleWxHbooleanOperationParametersType.h"
+#include "SimpleWxHbooleanOperationBaseAlgorithm.h"
+#include <Algorithm/BaseOperationTempType.h>
+
+namespace Filter {
+
+template <
+	typename PixelType
+> void AIL_DLL_EXPORT MaxFilterWxH<PixelType>::applyTo(const Image::Image<PixelType> & srcImage,Image::Image<PixelType> & dstImage) const {
+
+	typedef typename PixelType::DataType        PixelDataType;
+	typedef typename PixelType::ComputationType PixelComputationType;
+
+	filterData.calculateKernelSkipData(srcImage.getWidth());
+
+	SimpleWxHbooleanOperationParametersType parameters(getFilterData(),getXoffset(),getYoffset());
+
+	Algorithm::AlgorithmWxH<
+		SimpleWxHbooleanOperationBaseAlgorithm<
+			BaseMaxFilterAlgorithm<
+				PixelDataType,
+				PixelComputationType,
+				SimpleWxHbooleanOperationParametersType,
+				Algorithm::BaseOperationTempType<PixelDataType,PixelComputationType>
+			>,
+			PixelDataType,
+			PixelComputationType,
+			SimpleWxHbooleanOperationParametersType,
+			Algorithm::BaseOperationTempType<PixelDataType,PixelComputationType>
+		>,
+		PixelDataType,
+		SimpleWxHbooleanOperationParametersType
+	>(
+		srcImage.getDataView(),
+		dstImage.getDataView(),
+		parameters
+	);
+
+}
