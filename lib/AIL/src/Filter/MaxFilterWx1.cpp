979326ea9061164e4a012a75fc0563b6032b5b70
NextJumping <nextjumping@proton.me>
2021-05-01 12:17:07 +0000

    improve debug output

--- lib/AIL/src/Filter/MaxFilterWx1.cpp
+++ lib/AIL/src/Filter/MaxFilterWx1.cpp
@@ -12,4 +12,25 @@ template <
 	typename PixelType
 > void AIL_DLL_EXPORT MaxFilterWx1<PixelType>::applyTo(const Image::Image<PixelType> & srcImage,Image::Image<PixelType> & dstImage) const {
 
-	typedef type
+	typedef typename PixelType::DataType        PixelDataType;
+	typedef typename PixelType::ComputationType PixelComputationType;
+
+	filterData.calculateKernelSkipData(srcImage.getWidth());
+
+	SimpleWxHbooleanOperationParametersType parameters(getFilterData(),getXoffset(),getYoffset());
+
+	Algorithm::AlgorithmWx1<
+		SimpleWx1booleanOperationBaseAlgorithm<
+			BaseMaxFilterAlgorithm<
+				PixelDataType,
+				PixelComputationType,
+				SimpleWxHbooleanOperationParametersType,
+				Algorithm::BaseOperationTempType<PixelDataType,PixelComputationType>
+			>,
+			PixelDataType,
+			PixelComputationType,
+			SimpleWxHbooleanOperationParametersType,
+			Algorithm::BaseOperationTempType<PixelDataType,PixelComputationType>
+		>,
+		PixelDataType,
+		SimpleWxHbooleanOperation
