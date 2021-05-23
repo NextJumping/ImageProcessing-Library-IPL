1bc2776b8be98db3cccc2f782e081900d903f13b
NextJumping <nextjumping@proton.me>
2021-05-23 16:24:11 +0000

    prepare for next development iteration

--- lib/AIL/src/Filter/SimpleWx1booleanOperationBaseAlgorithm.h
+++ lib/AIL/src/Filter/SimpleWx1booleanOperationBaseAlgorithm.h
@@ -12,4 +12,26 @@ template <
 	typename PixelComputationType,
 	typename ParametersType,
 	typename TempType
-> class SimpleWx1boolean
+> class SimpleWx1booleanOperationBaseAlgorithm {
+	public:
+		static FINLINE void process(
+			PixelDataType * const & dstImageDataPtr,
+			const Image::ImageView<PixelDataType> & srcImage,
+			const ParametersType & parameters,
+			const I4 & x,
+			const I4 & y)
+		{
+			TempType tempData;
+			DerivedAlgorithmType::initial(tempData,parameters);
+			auto filterDataPtr = parameters.filterDataPtr;
+			I4 xf=x-parameters.xOffset;
+			for(;xf<x+(parameters.filterWidth-parameters.xOffset); ++xf){
+				if(Pixel::BooleanTestType::isIncluded((*filterDataPtr))==true){
+					DerivedAlgorithmType::first(tempData,parameters,srcImage.getPixel(xf,y).getAsComp<PixelComputationType::NumberType>());
+					++filterDataPtr;
+					break;
+				}
+				++filterDataPtr;
+			}
+			for(;xf<x+(parameters.filterWidth-parameters.xOffset); ++xf){
+				if(Pixel::BooleanTestType
