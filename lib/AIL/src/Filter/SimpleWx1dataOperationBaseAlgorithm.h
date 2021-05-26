aaa407aa2205018effa5261f8905f60f9a870a1d
NextJumping <nextjumping@proton.me>
2021-05-26 15:37:48 +0000

    Color fix

--- lib/AIL/src/Filter/SimpleWx1dataOperationBaseAlgorithm.h
+++ lib/AIL/src/Filter/SimpleWx1dataOperationBaseAlgorithm.h
@@ -28,4 +28,19 @@ template <
 			DerivedAlgorithmType::first(tempData,parameters,srcImage.getPixel(xf,y).getAsComp<PixelComputationType::NumberType>(),(*filterDataPtr).getAsComp<PixelComputationType::NumberType>());
 			++filterDataPtr;++xf;
 			for(;xf<x+(parameters.filterWidth-parameters.xOffset); ++xf){
-				DerivedAlgorithmType::inner(tempData,param
+				DerivedAlgorithmType::inner(tempData,parameters,srcImage.getPixel(xf,y).getAsComp<PixelComputationType::NumberType>(),(*filterDataPtr).getAsComp<PixelComputationType::NumberType>());
+				++filterDataPtr;
+			}
+			DerivedAlgorithmType::final(tempData,parameters);
+			(*dstImageDataPtr)=tempData.resultPixel;
+		}
+		static FINLINE void process(
+			PixelDataType * const & dstImageDataPtr,
+			const PixelDataType * const & srcImageDataPtrIn,
+			const ParametersType & parameters)
+		{
+			TempType tempData;
+			DerivedAlgorithmType::initial(tempData,parameters);
+			auto srcImageDataPtr = srcImageDataPtrIn;
+			auto filterDataPtr   = parameters.filterDataPtr;
+			DerivedAlgorithmType::first(tempData,parameters,(*srcImageDataPtr).getAsComp<PixelComputationType::Numbe
