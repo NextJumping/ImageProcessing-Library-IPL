75face5627878bff25d78a0358697969a7cf6ad9
NextJumping <nextjumping@proton.me>
2021-05-27 16:53:24 +0000

    merge files

--- lib/AIL/src/Filter/SimpleWxHbooleanOperation.h
+++ lib/AIL/src/Filter/SimpleWxHbooleanOperation.h
@@ -11,4 +11,24 @@ namespace Filter {
 
 template <
 	typename PixelType
-> 
+> class AIL_DLL_EXPORT SimpleWxHbooleanOperation
+	: public ImageOperation<PixelType>
+{
+	protected:
+		typedef Image::ImageKernel<Pixel::PixelYb1,Pixel::BooleanTestType> FilterDataType;
+		mutable FilterDataType filterData;
+		I4 xOffset;
+		I4 yOffset;
+
+	public:
+
+		FINLINE const FilterDataType & getFilterData() const {return filterData;}
+
+		FINLINE const I4 & getXoffset() const {return xOffset;}
+		FINLINE const I4 & getYoffset() const {return yOffset;}
+
+		FINLINE void setXoffset(const I4 & _xOffset){xOffset=_xOffset;}
+		FINLINE void setYoffset(const I4 & _yOffset){yOffset=_yOffset;}
+
+		FINLINE       B1 & operator()(const I4 & x,const I4 & y)       {return filterData(x,y);};
+		FINLINE const B1 & operator()(const I4 & x,const I4 & y) const {
