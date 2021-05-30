f5ab0b33e33a4e8046294195e7fc1fdc58266654
NextJumping <nextjumping@proton.me>
2021-05-30 21:02:30 +0000

    dont need this

--- lib/AIL/src/Filter/SimpleWxHdataOperation.h
+++ lib/AIL/src/Filter/SimpleWxHdataOperation.h
@@ -4,4 +4,29 @@
 
 #include "AIL.h"
 #include <Image/Image.h>
-#include <Filter/ImageOperation.
+#include <Filter/ImageOperation.h>
+
+namespace Filter {
+
+template <
+	typename PixelType
+> class AIL_DLL_EXPORT SimpleWxHdataOperation
+	: public ImageOperation<PixelType>
+{
+	protected:
+		Image::Image<PixelType> filterData;
+		I4 xOffset;
+		I4 yOffset;
+
+	public:
+
+		FINLINE const Image::Image<PixelType> &getFilterData() const {return filterData;}
+
+		FINLINE const I4 & getXoffset() const {return xOffset;}
+		FINLINE const I4 & getYoffset() const {return yOffset;}
+
+		FINLINE void setXoffset(const I4 & _xOffset){xOffset=_xOffset;}
+		FINLINE void setYoffset(const I4 & _yOffset){yOffset=_yOffset;}
+
+		FINLINE       PixelType & operator()(const I4 & x,const I4 & y)       {return filterData(x,y);};
+		FINLINE const PixelType & operator()(const I4 & x,const I4 & y) const {return filterData(x,y)
