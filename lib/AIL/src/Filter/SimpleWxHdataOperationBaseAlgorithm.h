7cea0fb8237328aaaaf6e9ceb9db5549d20e3a23
NextJumping <nextjumping@proton.me>
2021-05-31 14:40:45 +0000

    LICENSE.txt

--- lib/AIL/src/Filter/SimpleWxHdataOperationBaseAlgorithm.h
+++ lib/AIL/src/Filter/SimpleWxHdataOperationBaseAlgorithm.h
@@ -12,4 +12,21 @@ template <
 	typename PixelComputationType,
 	typename ParametersType,
 	typename TempType
-> class SimpleWxHd
+> class SimpleWxHdataOperationBaseAlgorithm {
+	public:
+		static FINLINE void process(
+			PixelDataType * const & dstImageDataPtr,
+			const Image::ImageView<PixelDataType> & srcImage,
+			const ParametersType & parameters,
+			const I4 & x,
+			const I4 & y)
+		{
+			TempType tempData;
+			DerivedAlgorithmType::initial(tempData,parameters);
+			auto filterDataPtr = parameters.filterDataPtr;
+			I4 yf=y-parameters.yOffset;
+			I4 xf=x-parameters.xOffset;
+			DerivedAlgorithmType::first(tempData,parameters,srcImage.getPixel(xf,yf).getAsComp<PixelComputationType::NumberType>(),(*filterDataPtr).getAsComp<PixelComputationType::NumberType>());
+			++filterDataPtr;++xf;
+			for(;xf<x+(parameters.filterWidth-parameters.xOffset); ++xf){
+				Deriv
