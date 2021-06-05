c3dd5aa69ef36e3d541e22fe02ad00cf0c6f8c12
NextJumping <nextjumping@proton.me>
2021-06-05 18:36:06 +0000

    baidu

--- lib/AIL/src/Filter/SimpleWxHdataOperationParametersType.h
+++ lib/AIL/src/Filter/SimpleWxHdataOperationParametersType.h
@@ -9,4 +9,39 @@ namespace Filter {
 
 template <
 	typename PixelDataType
-> class SimpleWxHdataOperati
+> class SimpleWxHdataOperationParametersType
+	: public Image::BaseAlgorithmWxHparametersType
+{
+	public:
+
+		SimpleWxHdataOperationParametersType(
+			const Image::ImageView<PixelDataType> & _filter,
+			const I4 & _xOffset,
+			const I4 & _yOffset,
+			const I4 & _srcImageWidth
+		)
+			:BaseAlgorithmWxHparametersType(
+				_xOffset,
+				_yOffset,
+				_filter.getWidth(),
+				_filter.getHeight())
+
+			,filterDataPtr   (_filter.getDataPtr())
+			,filterDataPtrEnd(_filter.getDataPtrEnd())
+
+			,srcImageWidth(_srcImageWidth)
+			,srcImageWidth_filterWidth(_srcImageWidth - _filter.getWidth())
+		{
+		}
+
+		const PixelDataType * const filterDataPtr;
+		const PixelDataType * const filterDataPtrEnd;
+
+		const I4 srcImageWidth;
+		const I4 srcImageWidth_filterWidth;
+
+};
+
+}
+
+#endif
