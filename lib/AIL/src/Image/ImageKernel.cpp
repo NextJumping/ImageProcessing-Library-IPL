843ae8cc8a359f48748afb0e5258d029ed52978d
NextJumping <nextjumping@proton.me>
2021-06-11 16:43:43 +0000

    Update appveyor.bat

--- lib/AIL/src/Image/ImageKernel.cpp
+++ lib/AIL/src/Image/ImageKernel.cpp
@@ -7,4 +7,31 @@ namespace Image {
 
 template <
 	typename PixelType,
-	typename Inclusi
+	typename InclusionTestType
+> ImageKernel<PixelType,InclusionTestType>::ImageKernel(Image<PixelType> && _kernel)
+	:kernel(Meta::forward(_kernel))
+	,kernelSkipDataPtr(nullptr)
+	,kernelSkipDataPtrEnd(nullptr)
+	,kernelSize(0)
+	,parentImageWidth(0)
+{
+}
+
+template <
+	typename PixelType,
+	typename InclusionTestType
+> ImageKernel<PixelType,InclusionTestType>::ImageKernel(Image<PixelType> && _kernel,const I4 & _parentImageWidth)
+	:kernel(Meta::forward(_kernel))
+	,kernelSkipDataPtr(nullptr)
+	,kernelSkipDataPtrEnd(nullptr)
+	,kernelSize(0)
+	,parentImageWidth(0)
+{
+	if(_parentImageWidth<kernel.getWidth()){return;} //TODO: Report an error
+	calculateKernelSkipData(_parentImageWidth);
+}
+
+template <
+	typename PixelType,
+	typename InclusionTestType
+> ImageKernel<Pixe
