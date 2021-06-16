aeca8977878f11721017528164ce0ac92ec119bf
NextJumping <nextjumping@proton.me>
2021-06-16 11:02:17 +0000

    Version bump to 2.2.1

--- lib/AIL/src/Image/ImageSize.h
+++ lib/AIL/src/Image/ImageSize.h
@@ -11,4 +11,27 @@ class AIL_DLL_EXPORT ImageSize {
 
 	public:
 		ImageSize();
-		ImageSize(const I4 & _width,const I4 & _height)
+		ImageSize(const I4 & _width,const I4 & _height);
+
+		typedef ImageSize ThisType;
+
+		FINLINE void set(const I4 & _width,const I4 & _height) {
+			width     = _width;
+			width_1   = _width - 1;
+			height    = _height;
+			height_1  = _height - 1;
+			numPixels = _width * _height;
+		}
+
+		FINLINE const I4 & getWidth()    const {return width   ;}
+		FINLINE const I4 & getWidth_1()  const {return width_1 ;}
+
+		FINLINE const I4 & getHeight()   const {return height   ;}
+		FINLINE const I4 & getHeight_1() const {return height_1 ;}
+		
+		FINLINE const I4 & getNumPixels() const {return numPixels;}
+
+		friend FINLINE bool operator<(const ThisType & other1, const ThisType & other2){
+			return (other1.getNumPixels() < other2.getNumPixels());
+		}
+		friend FINLINE bool o
