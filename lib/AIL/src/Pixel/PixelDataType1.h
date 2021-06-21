63adec7d9d64b96dd8b9b67e5c05cc4b44eef92e
NextJumping <nextjumping@proton.me>
2021-06-21 11:31:20 +0000

    Improve grammar

--- lib/AIL/src/Pixel/PixelDataType1.h
+++ lib/AIL/src/Pixel/PixelDataType1.h
@@ -14,4 +14,16 @@ template <
 		typedef NumberType                 NumberType;
 		typedef PixelDataType1<NumberType> ThisType;
 
-		FINLINE explicit Pixe
+		FINLINE explicit PixelDataType1(){}
+		FINLINE explicit PixelDataType1(const NumberType & _scalar):x(_scalar){}
+		FINLINE          PixelDataType1(const ThisType   & _pixel) :x(_pixel.x){}
+	
+		FINLINE ThisType & operator =(const ThisType   & _pixel ){x=_pixel.x;return (*this);}
+		FINLINE ThisType & operator =(const NumberType & _scalar){x=_scalar ;return (*this);}
+
+		FINLINE bool operator ==(const ThisType   & _pixel ) const {return (x==_pixel.x);}
+		FINLINE bool operator ==(const NumberType & _scalar) const {return (x==_scalar );}
+
+		template <typename DiffNumberType> FINLINE const void setComp(const PixelDataType1<DiffNumberType> & _pixel){x=_pixel.get<0>();};
+
+		template <typename DiffNumberType> FINLINE PixelDataType1<DiffNumberType> getAsComp() const {re
