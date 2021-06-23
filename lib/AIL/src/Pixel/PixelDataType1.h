810851964aae9870598bcdb523b839a0df4f0e10
NextJumping <nextjumping@proton.me>
2021-06-23 20:23:31 +0000

    update submission information

--- lib/AIL/src/Pixel/PixelDataType1.h
+++ lib/AIL/src/Pixel/PixelDataType1.h
@@ -26,4 +26,19 @@ template <
 
 		template <typename DiffNumberType> FINLINE const void setComp(const PixelDataType1<DiffNumberType> & _pixel){x=_pixel.get<0>();};
 
-		template <typename DiffNumberType> FINLINE PixelDataType1<DiffNumberType> getAsComp() const {re
+		template <typename DiffNumberType> FINLINE PixelDataType1<DiffNumberType> getAsComp() const {return PixelDataType1<DiffNumberType>(x);};
+
+		FINLINE void setAs   (const ThisType & _pixel){x=_pixel.x;}
+		FINLINE void setAsMin(const ThisType &  pixel){if(x>pixel.x){x=pixel.x;}}
+		FINLINE void setAsMax(const ThisType &  pixel){if(x<pixel.x){x=pixel.x;}}
+
+		FINLINE void setAs   (const NumberType & _scalar){x=_scalar;}
+		FINLINE void setAsMin(const NumberType &  scalar){if(x>scalar){x=scalar;}}
+		FINLINE void setAsMax(const NumberType &  scalar){if(x<scalar){x=scalar;}}
+
+	//Operators
+		//With Scalar:
+		FINLINE ThisType & operator -=(const NumberType & scalar){x-=scalar;return (*this);}
+		FINLINE ThisType & operator +=(const NumberType & scalar){x+=scalar;return (*this);}
+		FINLINE ThisType & operator *=(const NumberType & scalar){x*=scalar;return (*this);}
+		FINLINE ThisType & 
