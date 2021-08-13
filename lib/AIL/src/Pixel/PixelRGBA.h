bee9faacdbb2dc5e9a4c05766f43227cc87ae7df
NextJumping <nextjumping@proton.me>
2021-08-13 16:36:25 +0000

    index updates

--- lib/AIL/src/Pixel/PixelRGBA.h
+++ lib/AIL/src/Pixel/PixelRGBA.h
@@ -36,4 +36,16 @@ template <
 		FINLINE          PixelRGBAbaseType(const DataType   & _pixel ):ParentType(_pixel ){}
 		FINLINE          PixelRGBAbaseType(const ThisType   & _pixel ):ParentType(_pixel ){}
 
-		FINLINE explicit PixelRGBAbaseType(const NumberType & _r,const NumberType & _g,const NumberType & _b){setR(_r);setG(_
+		FINLINE explicit PixelRGBAbaseType(const NumberType & _r,const NumberType & _g,const NumberType & _b){setR(_r);setG(_g);setB(_b);setA(RangeType::maxValue);}
+		FINLINE explicit PixelRGBAbaseType(const NumberType & _r,const NumberType & _g,const NumberType & _b,const NumberType & _a){setR(_r);setG(_g);setB(_b);setA(_a);}
+		
+		FINLINE void setR(const NumberType & _r) {this->set<Rposition>(_r);}
+		FINLINE void setG(const NumberType & _g) {this->set<Gposition>(_g);}
+		FINLINE void setB(const NumberType & _b) {this->set<Bposition>(_b);}
+		FINLINE void setA(const NumberType & _a) {this->set<Aposition>(_a);}
+
+		FINLINE void clip(){RangeType::clip(*static_cast<ParentType*>(this));}
+
+		FINLINE const NumberType & getR() const {return this->get<Rposition>();}
+		FINLINE const NumberType & getG() const {return this->get<Gposition>();}
+		FINLINE const NumberType & getB() const {return this->get<Bposition>();}
