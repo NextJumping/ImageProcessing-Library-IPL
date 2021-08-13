b8858893a42130928e83d0c5d596c18efa9d90fc
NextJumping <nextjumping@proton.me>
2021-08-13 12:03:38 +0000

    Update notebooks.md

--- lib/AIL/src/Pixel/PixelRGBA.h
+++ lib/AIL/src/Pixel/PixelRGBA.h
@@ -27,4 +27,13 @@ template <
 		typedef typename NumberType     NumberType;
 		typedef typename PixelRangeType RangeType;
 		typedef PixelDataType3A<NumberType         ,Rposition,Gposition,Bposition,Aposition> DataType;
-		t
+		typedef PixelDataType3A<ComputationDataType,Rposition,Gposition,Bposition,Aposition> ComputationType;
+		typedef PixelDataType3A<NumberType         ,Rposition,Gposition,Bposition,Aposition> ParentType;
+		typedef PixelRGBAbaseType<NumberType,ComputationDataType,PixelRangeType,Rposition,Gposition,Bposition,Aposition> ThisType;
+
+		FINLINE explicit PixelRGBAbaseType(){}
+		FINLINE explicit PixelRGBAbaseType(const NumberType & _scalar):ParentType(_scalar){}
+		FINLINE          PixelRGBAbaseType(const DataType   & _pixel ):ParentType(_pixel ){}
+		FINLINE          PixelRGBAbaseType(const ThisType   & _pixel ):ParentType(_pixel ){}
+
+		FINLINE explicit PixelRGBAbaseType(const NumberType & _r,const NumberType & _g,const NumberType & _b){setR(_r);setG(_
