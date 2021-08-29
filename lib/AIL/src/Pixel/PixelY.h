dd5552936dbbef4e4fe4f6368683dc4594ee5d40
NextJumping <nextjumping@proton.me>
2021-08-29 07:15:10 +0000

    home button

--- lib/AIL/src/Pixel/PixelY.h
+++ lib/AIL/src/Pixel/PixelY.h
@@ -20,4 +20,21 @@ template <
 	public:
 
 		typedef typename NumberType                 NumberType;
-		typedef typename PixelRangeType             R
+		typedef typename PixelRangeType             RangeType;
+		typedef PixelDataType1<NumberType>          DataType;
+		typedef PixelDataType1<ComputationDataType> ComputationType;
+		typedef PixelDataType1<NumberType>          ParentType;
+		typedef PixelYbaseType<NumberType,ComputationDataType,PixelRangeType> ThisType;
+
+		FINLINE explicit PixelYbaseType(){}
+		FINLINE explicit PixelYbaseType(const NumberType & _scalar):ParentType(_scalar){}
+		FINLINE          PixelYbaseType(const DataType   & _pixel ):ParentType(_pixel ){}
+		FINLINE          PixelYbaseType(const ThisType   & _pixel ):ParentType(_pixel ){}
+
+		FINLINE void setY(const NumberType & _y) {this->set<0>(_y);}
+
+		FINLINE const NumberType & getY() const {return this->get<0>();}
+
+};
+
+typedef PixelYbaseType<B1 ,I4,PixelRange_1b1 > PixelYb1
