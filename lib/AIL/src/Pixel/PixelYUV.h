c301ae847fd638a756b2d5850177007b5f2749a2
NextJumping <nextjumping@proton.me>
2021-09-04 20:29:43 +0000

    drm/tilcdc: fix release order on exit

--- lib/AIL/src/Pixel/PixelYUV.h
+++ lib/AIL/src/Pixel/PixelYUV.h
@@ -15,3 +15,21 @@ template <
 	typename NumberType,
 	typename ComputationDataType,
 	typename PixelRangeType,
+	int Yposition,
+	int Uposition,
+	int Vposition
+> class AIL_DLL_EXPORT PixelYUVbaseType
+	: public PixelDataTypeY2<NumberType,Yposition>
+{
+	public:
+
+		typedef typename NumberType                            NumberType;
+		typedef typename PixelRangeType                        RangeType;
+		typedef PixelDataTypeY2<NumberType,Yposition>          DataType;
+		typedef PixelDataTypeY2<ComputationDataType,Yposition> ComputationType;
+		typedef PixelDataTypeY2<NumberType,Yposition>          ParentType;
+		typedef PixelYUVbaseType<NumberType,ComputationDataType,PixelRangeType,Yposition,Uposition,Vposition> ThisType;
+
+		FINLINE explicit PixelYUVbaseType(){}
+		FINLINE explicit PixelYUVbaseType(const NumberType & _scalar):ParentType(_scalar){}
+		FINLINE       
