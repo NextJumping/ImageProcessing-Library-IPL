f081c179b6b5048383dd43991199009e0c63b20b
NextJumping <nextjumping@proton.me>
2021-09-09 14:34:56 +0000

    VFS-285, ping driver test

--- lib/AIL/src/Pixel/pixel_cast.h
+++ lib/AIL/src/Pixel/pixel_cast.h
@@ -1,3 +1,28 @@
 
 #ifndef PIXEL__pixel_cast_H
-#define PIXEL__pixel_cast_
+#define PIXEL__pixel_cast_H
+
+#include "PixelY.h"
+#include "PixelRGB.h"
+#include "PixelRGBA.h"
+#include "PixelYUV.h"
+
+namespace Pixel {
+
+template <
+	typename DstPixelType,
+	typename SrcPixelType
+> class pixel_cast_alg {
+	public:
+		static FINLINE void process(DstPixelType * const & dstPixel,const SrcPixelType * const & srcPixel){
+			(*dstPixel) = pixel_cast<DstPixelType>(*srcPixel);
+		}
+};
+
+// TODO: split these into seperate header files
+template <typename DstPixelType,typename SrcPixelType> FINLINE DstPixelType pixel_cast(const SrcPixelType & color);
+
+template <> FINLINE PixelYb1 pixel_cast<PixelYb1>(const PixelYi1u & _color){
+	if(_color.getY()>128){return PixelYb1(true);}return PixelYb1(false);
+}
+template <> FINLINE PixelYb1 pixel_cast<PixelYb1>(const P
