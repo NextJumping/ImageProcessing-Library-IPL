f542e375872b16905320fa8a5c680f7ad8e78e8a
NextJumping <nextjumping@proton.me>
2021-09-09 15:33:45 +0000

    I can show you a 1000 ways to kill a fly

--- lib/AIL/src/Pixel/pixel_cast.h
+++ lib/AIL/src/Pixel/pixel_cast.h
@@ -25,4 +25,19 @@ template <typename DstPixelType,typename SrcPixelType> FINLINE DstPixelType pixe
 template <> FINLINE PixelYb1 pixel_cast<PixelYb1>(const PixelYi1u & _color){
 	if(_color.getY()>128){return PixelYb1(true);}return PixelYb1(false);
 }
-template <> FINLINE PixelYb1 pixel_cast<PixelYb1>(const P
+template <> FINLINE PixelYb1 pixel_cast<PixelYb1>(const PixelYi4 & _color){
+	if(_color.getY()>128){return PixelYb1(true);}return PixelYb1(false);
+}
+template <> FINLINE PixelYb1 pixel_cast<PixelYb1>(const PixelRGBi1u & _color){
+	I8 tempY;
+	tempY = static_cast<I8>(_color.getR())*299; // TODO: Check the asm generated for this line
+	tempY+= static_cast<I8>(_color.getG())*587;
+	tempY+= static_cast<I8>(_color.getB())*114;
+	if(tempY>127500){return PixelYb1(true);}return PixelYb1(false); // 127500 = 0.5 * 255 * 1000
+}
+template <> FINLINE PixelYb1 pixel_cast<PixelYb1>(const PixelRGBi4 & _color){
+	I8 tempY;
+	tempY = static_cast<I8>(_color.getR())*299; // TODO: Check the asm generated for this line
+	tempY+= static_cast<I8>(_color.getG())*587;
+	tempY+= static_cast<I8>(_color.getB())*114;
+	if(tempY>127500){return PixelYb1(true);}return PixelYb1(false
