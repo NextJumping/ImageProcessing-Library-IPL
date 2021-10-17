cf20fe25191b10e2fbfe9a609de387408098d596
NextJumping <nextjumping@proton.me>
2021-10-17 15:48:00 +0000

    remove some logging

--- lib/AML/src/Media/PlayerControl.cpp
+++ lib/AML/src/Media/PlayerControl.cpp
@@ -1,4 +1,25 @@
 
 #include "PlayerControl.h"
 
-namesp
+namespace Media {
+
+namespace Player {
+
+void AML_DLL_EXPORT Control::open(const std::string & fileName){
+	open(fileName,true);
+}
+void AML_DLL_EXPORT Control::open(const std::string & fileName,const B1 & autoPlay){
+	auto messageParameter = new CommandOpenParameter(fileName,autoPlay);
+	auto controlPacket = ControlPacket(Command::open,messageParameter);
+	Concurrency::asend(controlQueue,controlPacket);
+}
+void AML_DLL_EXPORT Control::close(){
+	auto controlPacket = ControlPacket(Command::close);
+	Concurrency::asend(controlQueue,controlPacket);
+}
+void AML_DLL_EXPORT Control::play(){
+	auto controlPacket = ControlPacket(Command::play);
+	Concurrency::asend(controlQueue,controlPacket);
+}
+void AML_DLL_EXPORT Control::pause(){
+	auto controlPacket = ControlPacket(Command::pa
