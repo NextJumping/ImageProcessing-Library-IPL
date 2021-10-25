c17ade36deb79638cc86db5d1af7d0d68b8f7b6f
NextJumping <nextjumping@proton.me>
2021-10-25 14:00:35 +0000

    sched: Implement lockless wake-queues

--- lib/AML_Qt/src/Media/FFMPEGqtPlayer.h
+++ lib/AML_Qt/src/Media/FFMPEGqtPlayer.h
@@ -15,3 +15,28 @@
 #include <Thread/QueuePipe.h>
 #include <Thread/QueueReaderAgent.h>
 #include <Media/PlayerControl.h>
+#include <Video/QueueQtTarget.h>
+#include <Audio/QueueQtTarget.h>
+#include <Audio/AudioQtDevice.h>
+#include <Media/FFMPEGmediaPlayerAgent.h>
+
+namespace Media {
+
+class AML_QT_DLL_EXPORT FFMPEGqtPlayer {
+	public:
+		FFMPEGqtPlayer(Data::DataManager * const _dataManager);
+		~FFMPEGqtPlayer();
+
+		void start(QAbstractVideoSurface * surface);
+		void stop();
+
+		Media::Player::Control * getMediaControl(){return mediaControl.get();}
+
+	private:
+		Data::DataManager * const dataManager;
+
+		Concurrency::unbounded_buffer<Media::Player::ControlPacket> controlQueue;
+		std::unique_ptr<Thread::Queue::Pipe> videoPipe;
+		std::unique_ptr<Thread::Queue::Pipe> audioPipe;
+		std::unique_ptr<Video::Queue::QtTarget> videoQtTarget;
+		std::unique_ptr<Audio::Queue::QtTar
