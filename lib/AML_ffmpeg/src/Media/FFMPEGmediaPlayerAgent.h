87981113f52b4759b8e357ab96ca7f2137cb426a
NextJumping <nextjumping@proton.me>
2021-11-30 19:15:41 +0000

    Add improvements

--- lib/AML_ffmpeg/src/Media/FFMPEGmediaPlayerAgent.h
+++ lib/AML_ffmpeg/src/Media/FFMPEGmediaPlayerAgent.h
@@ -19,4 +19,42 @@ class AML_FFMPEG_DLL_EXPORT FFMPEGmediaPlayerAgent : public Concurrency::agent {
 	public:
 		typedef FFMPEGmediaPlayerAgent ThisType;
 
-		FFMPEGmediaPlayerAgent(Data::DataManager * const & _dataManager,Concurrency::ISo
+		FFMPEGmediaPlayerAgent(Data::DataManager * const & _dataManager,Concurrency::ISource<Media::Player::ControlPacket> & _controlQueue,Thread::Queue::Pipe & _videoPipe,Thread::Queue::Pipe & _audioPipe);
+		~FFMPEGmediaPlayerAgent();
+
+	protected:
+		void run();
+
+	private:
+
+		enum class State {
+			playing,
+			paused,
+			stopped,
+			closed,
+		};
+
+		bool openFile(const std::string & fileName);
+		void closeFile();
+		Image::Image<Pixel::PixelRGBi1u> getFrame() const ;
+
+		void sendFlush();
+		void sendPause();
+		void sendPlay();
+		void sendQuit();
+
+		class FFMPEGmediaPlayerAgent_d;
+		FFMPEGmediaPlayerAgent_d * _this;
+
+		State state;
+		I8u videoFrameIndex;
+		I8u audioFrameIndex;
+
+		F8 lastVideoTimeStamp;
+		F8 lastVideoTimeDelta;
+		F8 lastAudioTimeStamp;
+		F8 lastAudioTimeDelta;
+		B1 resetVideoTime;
+		B1 resetAudioTime;
+
+		Data::DataManager * const dat
