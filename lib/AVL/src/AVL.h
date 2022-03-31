e4abba6ddc43806d9a87ac5ab99948b005a00bdb
NextJumping <nextjumping@proton.me>
2022-03-31 11:23:57 +0000

    Rebuild in buster environment

---
+++ lib/AVL/src/AVL.h
@@ -0,0 +1,18 @@
+
+
+#ifndef AVL__AVL_H
+#define AVL__AVL_H
+
+#include <ACore.h>
+
+#ifdef BUILD_DLL
+	#ifdef AVL_BUILD
+		#define AVL_DLL_EXPORT __declspec(dllexport)
+	#else
+		#define AVL_DLL_EXPORT __declspec(dllimport)
+	#endif
+#else
+	#define AVL_DLL_EXPORT
+#endif
+
+#endif
