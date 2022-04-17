e0481de1b2db0133e52987ba8226ff21128a19ff
NextJumping <nextjumping@proton.me>
2022-04-17 15:03:54 +0000

    braces

---
+++ lib/AVL_Qt/src/AVL_Qt.h
@@ -0,0 +1,18 @@
+
+
+#ifndef AVL_QT__AVL_Qt_H
+#define AVL_QT__AVL_Qt_H
+
+#include <ACore.h>
+
+#ifdef BUILD_DLL
+	#ifdef AVL_QT_BUILD
+		#define AVL_QT_DLL_EXPORT __declspec(dllexport)
+	#else
+		#define AVL_QT_DLL_EXPORT __declspec(dllimport)
+	#endif
+#else
+	#define AVL_QT_DLL_EXPORT
+#endif
+
+#endif
---
+++ lib/AVL_Qt/src/Video/QueueQtTarget.cpp
@@ -0,0 +1,13 @@
+
+
+#include "QueueQtTarget.h"
+
+#include <Data/DataTypes.h>
+#include <Video/Frame.h>
+#include <Video/Config.h>
+
+#include <QMetaType>
+
+namespace Video {
+
+namespace Queue {
