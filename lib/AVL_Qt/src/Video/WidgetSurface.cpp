8d97ac1489fe11b639398fc833c438c75763e743
NextJumping <nextjumping@proton.me>
2022-04-19 06:46:38 +0000

    add new datetinme

--- lib/AVL_Qt/src/Video/WidgetSurface.cpp
+++ lib/AVL_Qt/src/Video/WidgetSurface.cpp
@@ -21,4 +21,33 @@ QList<QVideoFrame::PixelFormat> AVL_QT_DLL_EXPORT WidgetSurface::supportedPixelF
 bool AVL_QT_DLL_EXPORT WidgetSurface::isFormatSupported(const QVideoSurfaceFormat & format, QVideoSurfaceFormat * similar) const {
 	Q_UNUSED(similar);
 
-	const QImage::Format imageFormat = QVideoFrame::imageFormatFromP
+	const QImage::Format imageFormat = QVideoFrame::imageFormatFromPixelFormat(format.pixelFormat());
+	const QSize size = format.frameSize();
+
+	return imageFormat != QImage::Format_Invalid
+		&& !size.isEmpty()
+		&& format.handleType() == QAbstractVideoBuffer::NoHandle;
+}
+
+bool AVL_QT_DLL_EXPORT WidgetSurface::start(const QVideoSurfaceFormat & format){
+	const QSize size = format.frameSize();
+
+	if (size.isEmpty()==false) {
+		imageSize.set(size.width(),size.height());
+		sourceRect = format.viewport();
+
+		QAbstractVideoSurface::start(format);
+
+		widget->updateGeometry();
+		updateVideoRect();
+
+		return true;
+	}else{
+		return false;
+	}
+}
+void AVL_QT_DLL_EXPORT WidgetSurface::updateVideoRect(){
+	QSize videoSize  = surfaceFormat().sizeHint();
+	QSize widgetSize = widget->size();
+
+	QSize targetSize = videoS
