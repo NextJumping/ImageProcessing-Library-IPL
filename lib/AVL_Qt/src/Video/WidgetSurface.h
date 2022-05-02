883241e44f2455ba4acc96e189eda19e2ca503be
NextJumping <nextjumping@proton.me>
2022-05-02 07:48:00 +0000

    Bump to version 1.0.2

--- lib/AVL_Qt/src/Video/WidgetSurface.h
+++ lib/AVL_Qt/src/Video/WidgetSurface.h
@@ -1,2 +1,30 @@
 
 #ifndef VIDEO__WidgetSurface_H
+#define VIDEO__WidgetSurface_H
+
+#include "AVL_Qt.h"
+
+#include <Data/DataTypes.h>
+#include <Image/Image.h>
+
+#include <QtCore/QRect>
+#include <QtGui/QImage>
+#include <QtMultimedia/QAbstractVideoSurface>
+#include <QtMultimedia/QVideoFrame>
+
+namespace Video {
+
+class AVL_QT_DLL_EXPORT WidgetSurface : public QAbstractVideoSurface {
+	Q_OBJECT
+	public:
+		WidgetSurface(QWidget * widget, QObject * parent = 0);
+
+		~WidgetSurface();
+
+		QList<QVideoFrame::PixelFormat> supportedPixelFormats(QAbstractVideoBuffer::HandleType handleType = QAbstractVideoBuffer::NoHandle) const;
+		bool isFormatSupported(const QVideoSurfaceFormat & format, QVideoSurfaceFormat * similar) const;
+
+		bool start(const QVideoSurfaceFormat & format);
+		void stop();
+
+		QRect videoRect() c
